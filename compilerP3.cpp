1302ac046320c63aad28a7fbccef6beae5154662
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-06-28 22:40:25 +0800

    Create compilerP3.cpp

---
+++ compilerP3.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+// vectors being named as (dType)List
+int iList(){
+  vector<int> i;
+    // continue..
+  return i;
+}
+
+double dList(){
+  vector<double> d;
+    // continue...
+  return d;
+}
+
+float fList(){
+  vector<float> f;
+    // continue..
+  return f;
+}
+
+long lList(){
+  vector<long> l;
+  // continue...
+  return l;
+}
+
+bool bList(){
+  vector<bool> b;
+    // continue..
+  return b;
+}
+
+char cList(){
+  vector<char> c;
+    // continue..
+  return c;
+}
+
+string sList(){
+  vector<string> s;
+    // continue..
+  return s;
+}
