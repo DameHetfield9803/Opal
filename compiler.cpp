f80f3b62c65748b5de2ef3852edc36d80157757a
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-06-28 20:23:32 +0800

    Update compiler.cpp

--- compiler.cpp
+++ compiler.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
+#include <string>
+// basically print() in Python
+string echo(string x){
+  std::cout << x; 
+}
+int echo(int i){
+  std::cout << i; 
+}
+int echo(char j){
+  std::cout << j;
+}
 
-int main(){
-  
+double echo(double a){
+  std::cout << a;
+}
+
+float echo(float b){
+  std::cout << b;
+}
+
+long echo(long c){
+  std::cout << c;
+}
+
+bool echo(bool d){
+  if(d==true){
+    std::cout << true;
+  }
+  else{
+    std::cout << false;
+  }
+
+  // Math operations 
+  // Kevin pls help 
 }
