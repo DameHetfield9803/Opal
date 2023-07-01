43b5835db7266c16c01d4df0e233b4ebbc1d2c4a
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-02 02:29:15 +0800

    Create types.cpp

---
+++ OpalAST/types.cpp
@@ -0,0 +1,19 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+
+#include <iostream>
+
+//buffer size which lexer will use to read block from a file
+#define BUFFER_SIZE 512
+
+typedef std::string lexeme_t;
+typedef std::string record_t;
+typedef std::string identifier_t;
+
+//location of token
+typedef struct{
+  int line;
+  int col;
+}loc_t;
+
+#endif
