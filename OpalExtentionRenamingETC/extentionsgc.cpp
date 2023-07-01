094419cabc778a4555203c7a2d741f1584ca3e67
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-01 12:26:52 +0800

    Add files via upload

---
+++ OpalExtentionRenamingETC/extentionsgc.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <string_view>
+#include <unordered_set>
+#include <memory>
+
+// A simple garbage collector that tracks all allocated objects and deletes them when they are no longer needed.
+class GarbageCollector {
+public:
+  void addObject(void* object) {
+    // Add the object to the set of allocated objects.
+    objects.insert(object);
+  }
+
+  void deleteObject(void* object) {
+    // Remove the object from the set of allocated objects.
+    objects.erase(object);
+    // Deallocate the object memory.
+    free(object);
+  }
+
+private:
+  std::unordered_set<void*> objects;
+};
+
+int main() {
+  // Choose a file extension that is not already in use by another programming language.
+  constexpr std::string_view fileExtension = "op";
+
+  // Create a file with the chosen file extension in binary mode.
+  std::ofstream file(std::string(fileExtension) + ".cpp", std::ios::binary);
+
+  // Write the code for your new programming language in the file.
+  constexpr std::string_view codeBlock = R"(
+#include <iostream>
+#include <memory>
+
+// Create a garbage collector object.
+GarbageCollector gc;
+
+int main() {
+  // Allocate an object using std::unique_ptr.
+  std::unique_ptr<int> object = std::make_unique<int>(100);
+
+  // Add the object to the garbage collector.
+  gc.addObject(object.get());
+
+  // Do something with the object.
+  std::cout << *object << std::endl;
+
+  // The object will be automatically deleted when it goes out of scope.
+
+  return 0;
+})";
+
+  // Write the entire code block to the file.
+  file << codeBlock;
+
+  // Test your new programming language by running it.
+  system(("g++ -o " + std::string(fileExtension) + " " + fileExtension + ".cpp").c_str());
+  system(("./" + std::string(fileExtension)).c_str());
+
+  // You should see the message "100" printed to the console.
+
+  return 0;
+}
