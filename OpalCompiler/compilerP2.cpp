9c54f95dfa215fa9a7d2c98495f3325cb3f0b4e1
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-03 03:01:38 +0800

    Update compilerP2.cpp

--- OpalCompiler/compilerP2.cpp
+++ OpalCompiler/compilerP2.cpp
@@ -1,42 +1,119 @@
-#include <string>
-#include <vector>
 #include <iostream>
+#include <vector>
 
-// logic stuff
-// similar to a C++ for loop
-void for(){
-  
+// A function that prints numbers from 'start' to 'end'
+void for(int start, int end) {
+  for (int i = start; i < end; ++i) {
+    std::cout << i << std::endl;
+  }
 }
-// similar to a C++ while loop
+
+// Method overloading for customForLoop to handle different data types
+template <typename T>
+void for(T start, T end) {
+  for (T i = start; i < end; ++i) {
+    std::cout << i << std::endl;
+  }
+}
+
+// A function that prints "True" while the condition is true
+void while(bool condition) {
+  while (condition) {
+    std::cout << "True" << std::endl;
+    // You need to change the condition inside the loop
+    // to eventually make it false; otherwise, it will result in an infinite loop.
+    condition = false;
+  }
+}
+
+// A function that prints numbers from 0 to 9
 void while() {
-  
+  int i = 0;
+  do {
+    std::cout << i << std::endl;
+    i++;
+  } while (i < 10);
 }
 
-// do condition while condition hasn't been met
-// aka a do while loop
-void do(){
-  
+// A function that prints elements of a vector
+void foreach(const std::vector<int>& numbers) {
+  for (int number : numbers) {
+    std::cout << number << std::endl;
+  }
 }
 
-// Took the idea from C# and am trying to implement this into C++
-void foreach(){
-  
+// Method overloading for customForEach to handle different data types
+template <typename T>
+void foreach(const std::vector<T>& elements) {
+  for (const T& element : elements) {
+    std::cout << element << std::endl;
+  }
 }
 
-// similar to a  C++ if statement
-void if(){
-  
-}
-// similar to a  C++ else if statement
-void else if(){
-  
-}
-// similar to a  C++ else statement
-void else(){
-  
+// A function that prints "True" if the condition is true, otherwise prints "False"
+void if(bool condition) {
+  if (condition) {
+    std::cout << "True" << std::endl;
+  } else {
+    std::cout << "False" << std::endl;
+  }
 }
 
-// Based on C++'s function's 
-void fn(){
-   // user defined function 
+// A function that prints "True" if the condition is true, otherwise does nothing
+void else if(bool condition) {
+  if (condition) {
+    std::cout << "True" << std::endl;
+  }
 }
+
+// A function that prints "Else"
+void else() {
+  if (!customIf(false)) {
+    std::cout << "Else" << std::endl;
+  }
+}
+
+// A function that prints numbers from 0 to n-1
+void fn(int n) {
+  for (int i = 0; i < n; ++i) {
+    std::cout << i << std::endl;
+  }
+}
+
+// Method overloading for customFunction to handle different data types
+template <typename T>
+void fn(const T& n) {
+  for (T i = 0; i < n; ++i) {
+    std::cout << i << std::endl;
+  }
+}
+
+int main() {
+  customForLoop(0, 5);
+  std::cout << "--------" << std::endl;
+
+  customForLoop('a', 'e');
+  std::cout << "--------" << std::endl;
+
+  customWhileLoop(true);
+  std::cout << "--------" << std::endl;
+
+  customDoWhileLoop();
+  std::cout << "--------" << std::endl;
+
+  std::vector<int> numbers = {1, 2, 3, 4, 5};
+  customForEach(numbers);
+  std::cout << "--------" << std::endl;
+
+  std::vector<char> chars = {'a', 'b', 'c', 'd', 'e'};
+  customForEach(chars);
+  std::cout << "--------" << std::endl;
+
+  customIf(true);
+  std::cout << "--------" << std::endl;
+
+  customElseIf(false);
+  std::cout << "--------" << std::endl;
+
+  customElse();
+  std::cout << "--------" << std::endl
