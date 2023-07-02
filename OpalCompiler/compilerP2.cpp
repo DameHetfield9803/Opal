7f14c2ffd365e4303762f8e2f3e50e66c783f189
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-03 02:44:05 +0800

    Update compilerP2.cpp

--- OpalCompiler/compilerP2.cpp
+++ OpalCompiler/compilerP2.cpp
@@ -3,27 +3,35 @@
 #include <iostream>
 
 // logic stuff
-
+// similar to a typical C++ for loop
 void for(){
   
 }
-
+// similar to a typical C++ while loop
 void while() {
   
 }
 
+// do condition while condition hasn't been met
+// aka a typical do while loop
 void do(){
   
 }
 
+// Took the idea from C# and am trying to implement this into C++
+void foreach(){
+  
+}
+
+// similar to a typical C++ if statement
 void if(){
   
 }
-
+// similar to a typical C++ else if statement
 void else if(){
   
 }
-
+// similar to a typical C++ else statement
 void else(){
   
 }
