177bd81755c6a39afbace2a24711e5152cde17ad
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-04 01:20:39 +0800

    Update compilerP3.cpp

--- OpalCompiler/compilerP3.cpp
+++ OpalCompiler/compilerP3.cpp
@@ -2,44 +2,12 @@
 #include <vector>
 #include <string>
 
-// vectors being named as (dType)List
-uint64 iList(){
-  vector<int> i;
-    // Initialises a uint64 vector, which in this case is called a list
-  return i;
-}
-
-double dList(){
-  vector<double> d;
-  // Initialises a double vector, which in this case is called a list 
-}
-
-float fList(){
-  vector<float> f;
-    // Initialises a float vector, which in this case is called a flist
-  return f;
-}
-
-long lList(){
-  vector<long> l;
-  // Initialises a long vector, which in this case is called a list
-  return l;
-}
-
-bool bList(){
-  vector<bool> b;
-   // Initialises a boolean vector, which in this case is called a list
-  return b;
-}
-
-char cList(){
-  vector<char> c;
-   // Initialises a char vector, which in this case is called a list
-  return c;
-}
-
-string sList(){
-  vector<string> s;
-  // Initialises a string vector, which in this case is called a list
-  return s;
+using namespace std;
+// Uses RVO (Return Value Optimization used in the return {})
+// uses template to make it so that only explicit types that are valid will be allowed
+// Basically a free type checker (you can think of it this way)
+template <typename T>
+vector<T> aList() {
+  // aList just stands for ArrayList in Java 
+  return {};
 }
