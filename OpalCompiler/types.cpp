cc18d3e40c252360ba52260a8ddf686b39e503ea
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-04 02:08:46 +0800

    Create types.cpp

---
+++ OpalCompiler/types.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+typedef uint64_t int;
+typedef long l;
+typedef float f;
+typedef char c;
+typedef string s;
+typedef double dbl;
+typedef bool bln;
+// For defining types (... as name implies)
