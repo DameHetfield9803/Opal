bc19895e43ad36653c25b4b19846fc341f264acf
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-04 01:33:41 +0800

    Update compilerP4.cpp

--- OpalCompiler/compilerP4.cpp
+++ OpalCompiler/compilerP4.cpp
@@ -1,4 +1,27 @@
+// This part will be used for Garbage Collection
 #include <iostream>
 #include <vector>
-#include <memory>
+#include <memory>  // Added for smart pointer usage
 #include <stdexcept>
+
+using namespace std;
+
+class GarbageCollector {
+public:
+  void AddObject(unique_ptr<int> object) {
+    objects.push_back(move(object));
+  }
+
+  void CollectGarbage() {
+    for (auto it = objects.begin(); it != objects.end();) {
+      if (*it == nullptr) {
+        it = objects.erase(it);
+      } else {
+        it++;
+      }
+    }
+  }
+
+private:
+  vector<unique_ptr<int>> objects;
+};
