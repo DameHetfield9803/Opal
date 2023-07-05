ef4b1a843b00e2bfec4e34dd4df05559b41c0828
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-05 20:26:15 +0800

    Update extentionsgc.cpp
    idk how good this is so i just commented the previous code

--- OpalExtentionRenamingETC/extentionsgc.cpp
+++ OpalExtentionRenamingETC/extentionsgc.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 #include <fstream>
 #include <string_view>
 #include <unordered_set>
@@ -78,4 +78,91 @@ int main() {
   }
 
   return 0;
+}
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <memory>
+#include <unordered_map>
+
+// A simple garbage collector that tracks all allocated objects and deletes them when they are no longer needed.
+// The only reason the string_view is giving an error is because apparently THIS FUCKING VERSION OF C++ IN GITHUB AND VSCODE IS TOO FUCKING OLD WTF
+// Fret not it is not a syntax or wtv tf error it's just vscode and the inbuilt C++ extention
+class GarbageCollector {
+public:
+    void addObject(std::unique_ptr<void> object) {
+        // Add the object to the map of allocated objects.
+        objects[object.get()] = std::move(object);
+    }
+
+    void deleteObject(void* object) {
+        // Remove the object from the map of allocated objects.
+        objects.erase(object);
+    }
+
+private:
+    std::unordered_map<void*, std::unique_ptr<void>> objects;
+};
+
+void createAndRunNewLanguage() {
+    // Choose a file extension that is not already in use by another programming language.
+    const std::string fileExtension = "opal";
+
+    // Create a file with the chosen file extension in binary mode.
+    std::ofstream file(fileExtension + ".cpp", std::ios::binary);
+
+    // Write the code for your new programming language in the file.
+    const std::string codeBlock = R"(
+#include <iostream>
+#include <memory>
+
+// Create a garbage collector object.
+GarbageCollector gc;
+
+int main() {
+  // Allocate an object using std::make_unique.
+  auto object = std::make_unique<int>(100);
+
+  // Add the object to the garbage collector.
+  gc.addObject(std::move(object));
+
+  // Do something with the object.
+  std::cout << *object << std::endl;
+
+  // The object will be automatically deleted when it goes out of scope.
+
+  return 0;
+})";
+
+    // Write the entire code block to the file.
+    file << codeBlock;
+
+    // Test your new programming language by running it.
+    system(("g++ -o " + fileExtension + " " + fileExtension + ".cpp").c_str());
+    system(("./" + fileExtension).c_str());
+
+    // You should see the message "100" printed to the console.
+}
+
+int main() {
+    // Create a garbage collector object.
+    GarbageCollector gc;
+
+    // Allocate an object using std::make_unique.
+    auto object = std::make_unique<int>(100);
+
+    // Add the object to the garbage collector.
+    gc.addObject(std::move(object));
+
+    // Do something with the object.
+    std::cout << *object << std::endl;
+
+    // The object will be automatically deleted when it goes out of scope.
+
+    // Call the function to create and run your new programming language.
+    createAndRunNewLanguage();
+
+    return 0;
 }
