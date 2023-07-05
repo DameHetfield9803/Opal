73a141bd015a0c98c64340c583d72fb413e02dd5
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-05 20:39:10 +0800

    Update types.cpp
    Changed to user define functions instead of using typedefs

--- OpalCompiler/types.cpp
+++ OpalCompiler/types.cpp
@@ -3,12 +3,39 @@
 
 #include <iostream>
 #include <string>
-
-typedef uint64_t int;
-typedef long l;
-typedef float f;
-typedef char c;
-typedef string s;
-typedef double dbl;
-typedef bool bln;
+// typedef uint64_t int;
+uint64_t int(int x){
+  this->x = x;
+  return 1;
+}
+//typedef long l;
+long long(long y){
+  this->y = y;
+  return 1;
+}
+//typedef float f;
+float float(float z){
+  this->z = z;
+  return 1;
+}
+//typedef char c;
+char char(char a){
+  this->a = a;
+  return 1;
+}
+//typedef string s;
+string string(string j){
+  this->j = j;
+  return 1;
+}
+//typedef double dbl;
+double double(double i){
+  this->i = i;
+  return 1;
+}
+//typedef bool bln;
+bool bln(bool k){
+  this->k = k;
+  return 1;
+}
 // For defining types (... as name implies)
