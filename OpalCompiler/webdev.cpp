8c256632041a7b9b6443d3769df376009e0eeb8f
kevinlem0n <126497052+kevin-lem0n@users.noreply.github.com>
2023-07-06 02:02:14 +0800

    Added structure for Routing

--- OpalCompiler/webdev.cpp
+++ OpalCompiler/webdev.cpp
@@ -1,5 +1,6 @@
 #include <crow.h>
 #include <iostream>
+#include <string>
 //PLS HELP IDK HOW GOOD THIS IS
 int main() {
     // Create a Crow application
@@ -15,10 +16,47 @@ int main() {
         return "Hello, World!";
     });
 
-    // Route for the "/about" path
-    CROW_ROUTE(app, "/about")
+    /*
+    - Routing syntax
+    Controller/ Action/ Index
+
+    localhost:3000/Product/Edit/abc
+
+    Controller: Product
+    Action: Edit
+    Id (dynamic value): abc
+    */
+
+    // Access control for Route
+    /*
+    E.g. only Admin role can access
+
+    ROUTE_AUTH("admin", "/admin/report");
+    */
+    void ROUTE_AUTH(const std::string& str, const std::string& str) {
+        // Logic goes here
+    }
+   
+    // Route for the "/books" path
+    CROW_ROUTE(app, "/books")
     ([](){
         return "This is the About page.";
+        // Some logic goes here...
+    });
+
+    // Route for the "/books/edit/id" path
+    // :id here is dynamic, value will change
+    CROW_ROUTE(app, "/book/edit/:id")
+    ([](){
+        return "This is the About page.";
+        // Some logic goes here...
+    });
+
+    // Route for the 404 page.
+    // If none of the routes are matched
+    CROW_ROUTE(app, "/404")
+    ([](){
+        return "404. Page not found.";
     });
 
     // Bind to the address and port, and enable multithreading
