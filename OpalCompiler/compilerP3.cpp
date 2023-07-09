44e8699ad09d000c756bf85e4dc28f773468e736
DameHetfield9803 <135241537+DameHetfield9803@users.noreply.github.com>
2023-07-09 14:00:37 +0800

    Update compilerP3.cpp

--- OpalCompiler/compilerP3.cpp
+++ OpalCompiler/compilerP3.cpp
@@ -3,11 +3,16 @@
 #include <string>
 
 using namespace std;
+
 // Uses RVO (Return Value Optimization used in the return {})
-// uses template to make it so that only explicit types that are valid will be allowed
+// Uses template to make it so that only explicit types that are valid will be allowed
 // Basically a free type checker (you can think of it this way)
 template <typename T>
-vector<T> aList() {
-  // aList just stands for ArrayList in Java 
-  return {};
+vector<T> aList(size_t size) {
+  // aList stands for ArrayList
+  // Instead of using the constructor initializer list {}, we can use vector reserve
+  // to pre-allocate memory for better efficiency
+  vector<T> result;
+  result.reserve(size); // Adjust the number based on your expected size
+  return result;
 }
